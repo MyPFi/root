45049844bad3995d27c66d40750b4f732979e4d4
Andrei Diego Cardoso <andreidiegocardoso@gmail.com>
2022-01-01 15:30:07 -0300

    Gives LearningApachePOIForExcelTest its own spreadsheet to play with.

--- code/stocks/src/test/scala/com/andreidiego/mpfi/stocks/exploratory/spreadsheets/excel/poi/LearningApachePOIForExcelTest.scala
+++ code/stocks/src/test/scala/com/andreidiego/mpfi/stocks/exploratory/spreadsheets/excel/poi/LearningApachePOIForExcelTest.scala
@@ -17,7 +17,7 @@ import scala.util.Try
 class LearningApachePOIForExcelTest extends FixtureAnyWordSpec :
   type FixtureParam = Sheet
 
-  val TEST_SPREADSHEET = "/com/andreidiego/mpfi/stocks/exploratory/spreasheets/excel/poi/spreadsheet.xlsx"
+  val TEST_SPREADSHEET = "LearningApachePOI.xlsx"
   val TEST_SHEET = "Learning Apache POI"
   val TRADING_DATE_COLUMN_INDEX = 0
   val TRADING_DATE_ENCODED = "40177"
