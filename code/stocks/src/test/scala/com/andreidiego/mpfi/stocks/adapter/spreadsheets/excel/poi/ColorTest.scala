351d940d61f1f9a876745d989cbf8803105f101f
Andrei Diego Cardoso <andreidiegocardoso@gmail.com>
2022-01-01 23:56:14 -0300

    Introduces the Color concept which is an enum of 141 named colors together with their hex and decimal RGB representations.

---
+++ code/stocks/src/test/scala/com/andreidiego/mpfi/stocks/adapter/spreadsheets/excel/poi/ColorTest.scala
@@ -0,0 +1,51 @@
+package com.andreidiego.mpfi.stocks.adapter.spreadsheets.excel.poi
+
+import org.scalatest.matchers.should.Matchers.*
+import org.scalatest.wordspec.AnyWordSpec
+
+import scala.language.deprecated.symbolLiterals
+
+class ColorTest extends AnyWordSpec :
+
+  def beCreatedFrom: AfterWord = afterWord("be created from")
+
+  "A Color" can beCreatedFrom {
+    "a hex RGB code" in {
+      Color("#CD5C5C") should not be None
+    }
+    "a decimal RGB code" in {
+      val RED = 205.toByte
+      val GREEN = 92.toByte
+      val BLUE = 92.toByte
+      Color(RED, GREEN, BLUE) should not be None
+    }
+    "one of its several named constants" in {
+      Color.Crimson should not be None
+    }
+  }
+  it should {
+    "not accept an hex RGB code that is not defined by one of its named constants" in {
+      Color("#BD2B3F") should be(None)
+    }
+    "not accept a decimal RGB code that is not defined by one of its named constants" in {
+      val RED = 251.toByte
+      val GREEN = 228.toByte
+      val BLUE = 213.toByte
+      Color(RED, GREEN, BLUE) should be(None)
+    }
+    "provide its decimal RGB representation" in {
+      Color.Crimson.dec should be(Array(220, 20, 60).map(_.toByte))
+    }
+    "provide its hex RGB representation" in {
+      Color.Crimson.hex should be("#DC143C")
+    }
+    "define the 141 named colors supported by modern browsers" in {
+      Color.values should have size 141
+    }
+    "have no duplicate hex value (other than the two pair of aliases Aqua/Cyan and Fuchsia/Magenta)among its named colors" in {
+      Color.values.map(_.hex).distinct.size should equal(Color.values.size - 2)
+    }
+    "have no duplicate dec value (other than the two pair of aliases Aqua/Cyan and Fuchsia/Magenta) among its named colors" in {
+      Color.values.map(_.dec.toSeq).distinct.size should equal(Color.values.size - 2)
+    }
+  }
