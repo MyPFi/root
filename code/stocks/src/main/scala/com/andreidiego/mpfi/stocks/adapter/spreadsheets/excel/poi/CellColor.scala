2150aea2ec59558d763c6173b79ffcb1629c0d85
Andrei Diego Cardoso <andreidiegocardoso@gmail.com>
2022-01-03 12:42:09 -0300

    Introduces the CellColor concept which is basically a case class that carries both the foreground and the backgronud collors af a cell.

---
+++ code/stocks/src/main/scala/com/andreidiego/mpfi/stocks/adapter/spreadsheets/excel/poi/CellColor.scala
@@ -0,0 +1,5 @@
+package com.andreidiego.mpfi.stocks.adapter.spreadsheets.excel.poi
+
+import com.andreidiego.mpfi.stocks.adapter.spreadsheets.excel.poi.Color.{Black, White}
+
+case class CellColor(foreground: Color = Black, background: Color = White)
