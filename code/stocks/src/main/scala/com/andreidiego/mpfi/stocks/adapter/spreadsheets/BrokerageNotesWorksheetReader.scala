a87b2d8460429d7b180076806a4771ba6f11eac2
Andrei Diego Cardoso <andreidiegocardoso@gmail.com>
2022-03-19 14:16:35 -0300

    Turns every non-'SummaryLine' into an 'Operation'.

--- code/stocks/src/main/scala/com/andreidiego/mpfi/stocks/adapter/spreadsheets/BrokerageNotesWorksheetReader.scala
+++ code/stocks/src/main/scala/com/andreidiego/mpfi/stocks/adapter/spreadsheets/BrokerageNotesWorksheetReader.scala
@@ -1,11 +1,35 @@
 package com.andreidiego.mpfi.stocks.adapter.spreadsheets
 
-import excel.poi.Worksheet
+import excel.poi.{Cell, Line, Worksheet}
 
 class BrokerageNotesWorksheetReader(val brokerageNotes: Seq[BrokerageNote])
 
-class BrokerageNote
+class BrokerageNote(val operations: Seq[Operation])
+
+class Operation
 
 object BrokerageNotesWorksheetReader:
+  private type Group = Seq[Line]
 
-  def from(worksheet: Worksheet): BrokerageNotesWorksheetReader = BrokerageNotesWorksheetReader(worksheet.groups.map(_ ⇒ BrokerageNote()))
+  private val FORMULA = "FORMULA"
+
+  def from(worksheet: Worksheet): BrokerageNotesWorksheetReader = BrokerageNotesWorksheetReader(
+    worksheet.groups.map(_.toBrokerageNote)
+  )
+
+  extension (group: Group)
+    private def toBrokerageNote: BrokerageNote = BrokerageNote(
+      group.filter(!_.isSummary).map(_ ⇒ Operation())
+    )
+
+  extension (line: Line)
+
+    private def isSummary: Boolean = nonEmptyCells.forall(isFormula)
+
+    private def nonEmptyCells: Seq[Cell] = line.cells.filter(nonEmpty)
+
+  extension (cell: Cell)
+
+    private def isFormula: Boolean = cell.`type` == FORMULA
+
+    private def nonEmpty: Boolean = cell.value.nonEmpty
--- code/stocks/src/test/scala/com/andreidiego/mpfi/stocks/adapter/spreadsheets/BrokerageNotesWorksheetReaderTest.scala
+++ code/stocks/src/test/scala/com/andreidiego/mpfi/stocks/adapter/spreadsheets/BrokerageNotesWorksheetReaderTest.scala
@@ -1,6 +1,6 @@
 package com.andreidiego.mpfi.stocks.adapter.spreadsheets
 
-import excel.poi.Worksheet
+import excel.poi.{Line, Worksheet}
 import org.apache.poi.openxml4j.opc.OPCPackage
 import org.apache.poi.xssf.usermodel.{XSSFWorkbook, XSSFWorkbookFactory}
 import org.scalatest.Outcome
@@ -30,17 +30,41 @@ class BrokerageNotesWorksheetReaderTest extends FixtureAnyFreeSpec :
 
       "BrokerageNotesWorksheetReader.from(worksheet)" should compile
     }
-    "turn every 'Group' into a 'BrokerageNote' when all 'Operations' in the 'Group' have the same 'TradingDate' and 'BrokerageNote'." in { poiWorkbook ⇒
-      val worksheet = Worksheet.from(poiWorkbook.getSheet("2")).get
-      assume(worksheet.groups.size == 4)
+    "turn every" - {
+      "'Group' into a 'BrokerageNote' when all 'Lines' in the 'Group' have the same 'TradingDate' and 'BrokerageNote'." in { poiWorkbook ⇒
+        val worksheet = Worksheet.from(poiWorkbook.getSheet("2")).get
+        assume(worksheet.groups.size == 4)
 
-      val brokerageNotes = BrokerageNotesWorksheetReader.from(worksheet).brokerageNotes
+        val brokerageNotes = BrokerageNotesWorksheetReader.from(worksheet).brokerageNotes
 
-      brokerageNotes should have size 4
+        brokerageNotes should have size 4
 
-      forAll(brokerageNotes)(_ shouldBe a[BrokerageNote])
+        forAll(brokerageNotes)(_ shouldBe a[BrokerageNote])
+      }
+      "non-'SummaryLine' into an 'Operation'" in { poiWorkbook ⇒
+        val worksheet = Worksheet.from(poiWorkbook.getSheet("2")).get
+        assume(worksheet.nonSummaryLines.size == 7)
+
+        val operations = BrokerageNotesWorksheetReader.from(worksheet).operations
+
+        operations should have size 7
+
+        forAll(operations)(_ shouldBe a[Operation])
+      }
     }
   }
 
 object BrokerageNotesWorksheetReaderTest:
   private val TEST_SPREADSHEET = "BrokerageNotes.xlsx"
+
+  private val FORMULA = "FORMULA"
+
+  extension (worksheet: Worksheet)
+
+    private def nonSummaryLines: Seq[Line] =
+      worksheet.groups.flatMap(_.filter(!_.cells.filter(_.value.nonEmpty).forall(_.`type` == FORMULA)))
+
+  extension (brokerageNotesWorksheetReader: BrokerageNotesWorksheetReader)
+
+    private def operations: Seq[Operation] =
+      brokerageNotesWorksheetReader.brokerageNotes.flatMap(_.operations)
