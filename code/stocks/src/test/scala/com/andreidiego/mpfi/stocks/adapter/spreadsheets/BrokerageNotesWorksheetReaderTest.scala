2a0d1fc24d2192fc158bb4291707d09b7e5bda47
Andrei Diego Cardoso <andreidiegocardoso@gmail.com>
2022-05-20 11:21:36 -0300

    Prevents a 'BrokerageNotesWorksheetReader' from being created when a 'Brokerage' contains extraneous characters that prevent it from being interpreted as a currency.

--- code/stocks/src/test/scala/com/andreidiego/mpfi/stocks/adapter/spreadsheets/BrokerageNotesWorksheetReaderTest.scala
+++ code/stocks/src/test/scala/com/andreidiego/mpfi/stocks/adapter/spreadsheets/BrokerageNotesWorksheetReaderTest.scala
@@ -332,6 +332,16 @@ class BrokerageNotesWorksheetReaderTest extends FixtureAnyFreeSpec :
                         s"A required attribute ('Brokerage') is missing on line '2' of 'Worksheet' '$TEST_SHEET_NAME'."
                       ))
                     }
+                    "when containing extraneous characters (anything other than numbers, a dot or comma, and currency symbols $ or R$)." in { poiWorkbook ⇒
+                      val TEST_SHEET_NAME = "BrokerageExtraneousChars"
+                      val TEST_SHEET = Worksheet.from(poiWorkbook.getSheet(TEST_SHEET_NAME)).get
+
+                      val errors = BrokerageNotesWorksheetReader.from(TEST_SHEET).errors
+
+                      errors should contain(UnexpectedContentType(
+                        s"'Brokerage' ('R$$ l5,99') on line '2' of 'Worksheet' '$TEST_SHEET_NAME' cannot be interpreted as a currency."
+                      ))
+                    }
                   }
                 }
               }
